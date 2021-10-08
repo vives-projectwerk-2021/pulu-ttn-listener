4474b104784d274e6c6bca1af2cb7fd9f9dd5f7d
Thomas Luca <thomasluca008@gmail.com>
2021-10-08 10:09:27 +0200

    fixed enviornment variables

--- src/api/data-post.js
+++ src/api/data-post.js
@@ -1,22 +1,22 @@
 const axios = require('axios')
+require('dotenv').config()
 
-// const api = axios.create({
-//     // baseURL: `${process.env.BACKEND_BASE_URL}:${process.env.BACKEND_PORT}`
-//     // baseURL: "http://localhost:3000/posts"
-// })
+const api = axios.create({
+    baseURL: `${process.env.BACKEND_BASE_URL}:${process.env.BACKEND_PORT}`
+})
 
 const Transmitter = {
     resource: 'posts',
 
     postPayload(payload) {
-        axios.post(`${process.env.BACKEND_BASE_URL}:${process.env.BACKEND_PORT}/posts`, payload)
-        .then(res => {
-          console.log(`statusCode: ${res.status}`)
-          console.log(res.data)
-          })
-        .catch(error => {
-          console.error(error)
+      api.post(`/${this.resource}`, payload)
+      .then(res => {
+        console.log(`statusCode: ${res.status}`)
+        console.log(res.data)
         })
+      .catch(error => {
+        console.error(error)
+      })
     }
 }
 
