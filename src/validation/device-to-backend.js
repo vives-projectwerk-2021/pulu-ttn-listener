98c9b3d9a3bc9ee6fc4c56a95ac045515ca72030
Falk Felix <felix.falk2903@gmail.com>
2021-11-05 16:57:02 +0100

    added a json schema for the json data that gets send to the backend

---
+++ src/validation/device-to-backend.js
@@ -0,0 +1,76 @@
+const DeviceToBackend = {
+
+    create: {
+        "type": "object",
+        "properties": {
+            "message": { "type": "string","required":true },
+            "data": {
+                "device_id": { "type": "string", "required":true },
+                "time": { "type": "string","required":true },
+                "count": { "type": "integer","required":true },
+                "sensors": {
+                    "battery": {
+                        "status": { "type": "integer" },
+                        "value": { "type": "integer" }
+                    },
+                    "light": {
+                        "status": { "type": "integer" },
+                        "value": { "type": "integer" }
+                    },
+                    "moisture": {
+                        "level1": {
+                            "status":{"type":"integer"},
+                            "value":{"type":"integer"}
+                        },
+                        "level2": {
+                            "status":{"type":"integer"},
+                            "value":{"type":"integer"}
+                        },
+                        "level3": {
+                            "status":{"type":"integer"},
+                            "value":{"type":"integer"}
+                        },
+                        "level4": {
+                            "status":{"type":"integer"},
+                            "value":{"type":"integer"}
+                        },
+                        "level5": {
+                            "status":{"type":"integer"},
+                            "value":{"type":"integer"}
+                        },
+                        "level6": {
+                            "status":{"type":"integer"},
+                            "value":{"type":"integer"}
+                        },
+                        "level7": {
+                            "status":{"type":"integer"},
+                            "value":{"type":"integer"}
+                        },
+                        "level8": {
+                            "status":{"type":"integer"},
+                            "value":{"type":"integer"}
+                        },
+                    },
+                    "temperature":{
+                        "air":{
+                            "status":{"type":"integer"},
+                            "value":{"type":"integer"}
+                        },
+                        "ground":{
+                            "status":{"type":"integer"},
+                            "value":{"type":"integer"}
+                        }
+                    }
+                },
+                "meta":{
+                    "gateway_cnt":{"type":"integer"},
+                    "strongest_rssi":{"type":"integeer"}
+                }
+            }
+        },
+        "additionalProperties": true
+        //RSSI might be added, so additionalProps is true for now
+    }
+}
+
+/* export { DeviceToBackend } */
